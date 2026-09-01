f76affa3adb8b1931d964686d6fbbb8daec821f0
greetingsfromniagara <greetingsfromniagara@gmail.com>
2026-09-01 12:12:54 -0400

    Force service worker update checks

--- app.js
+++ app.js
@@ -343,7 +343,7 @@
   });
 
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js').catch(() => {}));
+    window.addEventListener('load', () => navigator.serviceWorker.register('./sw.js?v=31', { updateViaCache: 'none' }).catch(() => {}));
   }
 
   renderStops();
